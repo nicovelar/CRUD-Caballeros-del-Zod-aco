a6ecdb3b6d36b13c20be2146b0ea575cbe624b0b
nicovelar <nicovelar@gmail.com>
2022-07-14 10:17:13 -0300

    Se implementa el método POST

--- main.js
+++ main.js
@@ -32,5 +32,31 @@ const getAll = async () => {
       $table.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`)
     }
   }
+d.addEventListener("DOMContentLoaded", getAll)
 
-  d.addEventListener("DOMContentLoaded", getAll)
+//A través del método POST creamos un nuevo santo
+d.addEventListener("submit", async e => {
+
+    if (e.target === $form) {
+      e.preventDefault()
+
+    if (!e.target.id.value) {
+
+        try {
+        let options = {
+        method: "POST",
+        headers: { "Content-type": "application/json; charset=utf-8" },
+        body: JSON.stringify({
+            nombre: e.target.name.value,
+            constelacion: e.target.constellation.value }) }
+
+        res = await fetch("http://localhost:3000/santos", options)
+        json = await res.json()
+
+        if (!res.ok) throw { status: res.status, statusText: res.statusText }
+        location.reload()
+
+        } catch (err) {
+          let message = err.statusText || "Ocurrió un error"
+          $form.insertAdjacentHTML("afterend", `<p><b>Error ${err.status}: ${message}</b></p>`)
+        } } } } )
